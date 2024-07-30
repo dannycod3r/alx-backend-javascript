bbc44b655d60b0ffd1fa28638b6bfb01db566b96
dannycod3r <oseid7807@gmail.com>
2024-07-30 09:53:58 +0000

    Task 5

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,58 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+
+// example usage
+const teacher1 = createEmployee(200);
+const director1 = createEmployee(1000);
+const director2 = createEmployee('$500');
+
+
+console.log(teacher1); // Should print Teacher
+console.log(director1); // Should print Director
+console.log(director2); // Should print Director
