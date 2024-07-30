b1a38e528e1d15f7c5041e1fc9ae5e3ba7b20fb6
dannycod3r <oseid7807@gmail.com>
2024-07-30 07:19:09 +0000

    Task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,6 +11,14 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
 // example usage
 const teacher3: Teacher = {
   firstName: 'John',
@@ -30,3 +38,4 @@ const director1: Directors = {
 
 console.log(teacher3);
 console.log(director1);
+console.log(printTeacher("John", "Doe"));
