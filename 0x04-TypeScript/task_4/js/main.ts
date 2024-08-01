0be4079a647438a3aa6838cf9fc45addd7b3cb15
dannycod3r <oseid7807@gmail.com>
2024-08-01 16:55:37 +0000

    Task 10

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,34 @@
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+import { Subjects } from './subjects/Cpp';
+import { Subjects as JavaSubjects } from './subjects/Java';
+import { Subjects as ReactSubjects } from './subjects/React';
+
+export const cpp = new Subjects.Cpp();
+export const java = new JavaSubjects.Java();
+export const react = new ReactSubjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10
+};
+
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
