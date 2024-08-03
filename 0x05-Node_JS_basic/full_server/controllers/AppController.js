093e31717b1fca7ff6e53d8846a0bf50a3cb9284
dannycod3r <oseid7807@gmail.com>
2024-08-03 12:44:44 +0000

    Task 8: Full server with express

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,9 @@
+// full_server/controllers/AppController.js
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
